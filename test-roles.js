68ba98ce004d2e757c3c8ee135ed6c2a7b21876d
Devaprakash J <devaprakashofficial@gmail.com>
2026-08-19 14:17:43 +0530

    chore: remove test scripts

--- test-roles.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://kjestbshetpglycmmrem.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZXN0YnNoZXRwZ2x5Y21tcmVtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NzA2MjIyNiwiZXhwIjoyMTAyNjM4MjI2fQ.tvbYzoSBKhJZgbBEwJSs2MhBRy4NAVtkKtyACdOlWmE');
-
-async function run() {
-  const { data } = await supabase.from('profiles').select('*').limit(5);
-  console.log('Sample profiles:', data);
-}
-run();
